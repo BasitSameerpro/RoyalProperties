f606fc72c0701ec85c955f16bb4e1d6c445c67db
BasitSameerpro <summerkhan054@gmail.com>
2024-12-10 15:15:14 +0500

    connected Mongo Database to the server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { userRoute } from './routes/userRoute.js';
+dotenv.config()
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors());
+
+app.listen(8000 , ()=>{
+    console.log(`Server is running on ${PORT}`);
+})
+
+app.use('/api/user' , userRoute)
