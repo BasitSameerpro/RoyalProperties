9883061242c04525ef003440c4375c48b71c34be
BasitSameerpro <summerkhan054@gmail.com>
2025-01-04 12:40:19 +0500

    made some changes

--- server/index.js
+++ server/index.js
@@ -11,9 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:3000', // Allow only requests from this frontend URL (port 3000)
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers in requests
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
   };
 app.use(express.json())
 app.use(cookieParser())
